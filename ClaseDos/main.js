4445aae129decbec14914b8db65b6844aaa27ab2
Paula Turner <minipaulitat@gmail.com>
2021-09-03 15:29:39 -0300

    clase dos, cambios

--- ClaseDos/main.js
+++ ClaseDos/main.js
@@ -1,8 +1,9 @@
 let nombre = prompt("Ingresá tu nombre");
 let apellido = prompt("Ingresá tu apellido");
-
-if ((nombre && apellido) != "") {
+if (nombre && apellido != "") {
   alert("Holis " + nombre + " " + apellido + " (ɔ◔‿◔)ɔ ♥");
-} else {
-  alert("No escribiste nada (ㆆ_ㆆ)");
+} else if (nombre == "" && apellido == "") {
+  alert("No escribiste nada ( ˘︹˘ )");
+} else if (nombre == "" || apellido == "") {
+  alert("Te faltó algo(ㆆ_ㆆ)");
 }
