600772335962f8303ed33e225ec239720ec37dcd
Paula Turner <minipaulitat@gmail.com>
2021-09-03 12:04:07 -0300

    clase dos

---
+++ ClaseDos/main.js
@@ -0,0 +1,8 @@
+let nombre = prompt("Ingresá tu nombre");
+let apellido = prompt("Ingresá tu apellido");
+
+if ((nombre && apellido) != "") {
+  alert("Holis " + nombre + " " + apellido + " (ɔ◔‿◔)ɔ ♥");
+} else {
+  alert("No escribiste nada (ㆆ_ㆆ)");
+}
