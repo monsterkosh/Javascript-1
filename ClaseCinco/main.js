64f43547b332f1f4c506217d1ec82e768a1cf01d
Paula Turner <minipaulitat@gmail.com>
2021-09-16 17:21:10 -0300

    cambios clase cinco

--- ClaseCinco/main.js
+++ ClaseCinco/main.js
@@ -1,3 +1,7 @@
+/*variables globales*/
+let cantidad1 = parseInt(prompt("ingrese cantidad de producto 1"));
+let cantidad2 = parseInt(prompt("ingrese cantidad de producto 2"));
+
 /*funciones globales*/
 const suma = (a, b) => a + b;
 const resta = (a, b) => a - b;
@@ -10,18 +14,14 @@ const montoTotal = (a, b) =>
     suma(a.descuento(), b.descuento())
   );
 function verification(param1, param2) {
-  while (isNaN(param1) || isNaN(param2)) {
+  if (isNaN(param1) || isNaN(param2)) {
     alert("solo numeros");
     cantidad1 = parseInt(prompt("ingrese cantidad de producto 1"));
     cantidad2 = parseInt(prompt("ingrese cantidad de producto 2"));
     verification(cantidad1, cantidad2);
-    break;
   }
 }
-/*variables globales*/
-let cantidad1 = parseInt(prompt("ingrese cantidad de producto 1"));
-let cantidad2 = parseInt(prompt("ingrese cantidad de producto 2"));
-
+verification(cantidad1, cantidad2);
 /*clases*/
 class producto {
   constructor(precio, cantidad) {
@@ -39,6 +39,5 @@ class producto {
 const producto1 = new producto(200, cantidad1);
 const producto2 = new producto(100, cantidad2);
 
-verification(cantidad1, cantidad2);
 let resultado = montoTotal(producto1, producto2);
 alert("su total es " + resultado);
