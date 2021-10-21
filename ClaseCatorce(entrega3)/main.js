6f5e89cdca528900ac1f3c6914e0f2497c24bdfb
Paula Turner <minipaulitat@gmail.com>
2021-10-21 18:38:23 -0300

    clase catorce

--- ClaseCatorce(entrega3)/main.js
+++ ClaseCatorce(entrega3)/main.js
@@ -12,41 +12,40 @@ class Producto {
     return this.precio * this.cantidad * 0.3;
   }
 }
+/*storage y JSON*/
+let guardarLocal = (clave, valor) => {
+  localStorage.setItem(clave, valor);
+};
 
+const almacenados = JSON.parse(localStorage.getItem("lista productos"));
+console.log(almacenados);
 /*objetos y arrays*/
 const productos = [];
 
-let botonp1 = document.getElementById("botonp1");
-let agregarProducto1 = () => {
-  let cantidad1 = parseInt(document.getElementById("qty1").value);
-  if (isNaN(cantidad1)) {
-    alert("ingrese nro válido");
-  }
+/*boton 1*/
+
+//let botonp1 = document.getElementById("botonp1");
+let cantidad1 = 0;
+$(`#botonp1`).click(() => {
+  cantidad1 += 1;
   productos.push(new Producto("agenda", 200, cantidad1));
   guardarLocal("lista productos", JSON.stringify(productos));
-  $(`#botonp1`).append(`<h5> Agenda agregada</h5>`);
-};
-
-botonp1.addEventListener("click", agregarProducto1);
-$(`#botonp1`).click(() => {
-  $(`#botonp1`).prop("disabled", true);
+  console.log("producto agregado");
+  Swal.fire("Producto agregado", "Producto agregado al carrito", "success");
 });
-let botonp2 = document.getElementById("botonp2");
 
-let agregarProducto2 = () => {
-  let cantidad2 = parseInt(document.getElementById("qty2").value);
-  if (isNaN(cantidad2)) {
-    alert("ingrese nro válido");
-  }
+/*boton 2*/
+
+let cantidad2 = 0;
+$(`#botonp2`).click(() => {
+  cantidad2 += 1;
+
   productos.push(new Producto("anotador", 100, cantidad2));
   guardarLocal("lista productos", JSON.stringify(productos));
-  $(`#botonp2`).append(`<h5> Anotador agregado</h5>`);
-};
-
-botonp2.addEventListener("click", agregarProducto2);
-$(`#botonp2`).click(() => {
-  $(`#botonp2`).prop("disabled", true);
+  console.log("producto agregado");
+  Swal.fire("Producto agregado", "Producto agregado al carrito", "success");
 });
+
 /*funciones globales*/
 const suma = (a, b) => a + b;
 const resta = (a, b) => a - b;
@@ -59,24 +58,22 @@ const montoTotal = (a, b) =>
     suma(a.descuento(), b.descuento())
   );
 
-/*storage y JSON*/
-let guardarLocal = (clave, valor) => {
-  localStorage.setItem(clave, valor);
-};
-
-const almacenados = JSON.parse(localStorage.getItem("lista productos"));
-console.log(almacenados);
-
 /*funcionamiento simulador*/
 $("#boton").on("click", () => {
   let resultado = montoTotal(productos[0], productos[1]);
   if ($(`#contCarrito`).length == 0) {
     $(`#carrito`).append(`<div id="contCarrito" class="text-center">
-  <h2>Carrito: </h2>
-<h4>${productos[0].nombre}: ${productos[0].cantidad} </h4>
-<h4>${productos[1].nombre}: ${productos[1].cantidad} </h4>
-  <h3>su total es ${resultado} </h3>
-  </div>`);
+    <h2 class="display-4">Carrito:</h2>
+    <div class="container">
+      <p>${productos[0].nombre}: ${productos[0].cantidad} </p>
+    </div>
+    <div class="container">
+      <p>${productos[1].nombre}: ${productos[1].cantidad} </p>
+    </div>
+    <div>
+      <h3>su total es ${resultado} </h3>
+    </div>
+      </div>`);
   }
 });
 
